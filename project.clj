69795c1b2bc71b5fa86c9b377b817c409dc853ac
rsclison <raymond.sclison@asso-cocktail.fr>
2024-09-18 11:42:07 +0200

    datom

--- project.clj
+++ project.clj
@@ -3,7 +3,21 @@
   :url "http://example.com/FIXME"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
-  :dependencies [[org.clojure/clojure "1.11.1"]]
+  :dependencies [[org.clojure/clojure "1.12.0"]
+                 [compojure "1.7.1"]
+                 [com.appsflyer/donkey "0.5.2"]
+                 [luposlip/json-schema "0.4.5"]
+                 [org.clojure/data.json "2.5.0"]
+                 [org.clojure/java.jdbc "0.7.12"]
+                 [clojure.java-time "1.4.2"]
+                 [org.clojure/core.cache "1.1.234"]
+                 [clj-http "3.13.0"]
+                 [puppetlabs/clj-ldap "0.4.1"]
+                 [hiccup "1.0.5"]
+                 [clj-json "0.5.3"]
+                 [com.h2database/h2 "2.3.232"]
+                 [com.brunobonacci/mulog "0.9.0"]
+                 [com.datomic/datomic-free "0.9.5697"]]
   :main ^:skip-aot autho.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all
---
+++ src/autho/datomic.clj
@@ -0,0 +1,73 @@
+(ns autho.datomic
+   (:require [datomic.api :as d])
+  (:import (java.io File))
+  )
+
+
+(def uri "datomic:free://resources/datomdb")
+
+;; Fonction pour initialiser la base de données si elle n'existe pas
+(defn init-db []
+  (when-not (d/create-database uri)
+    (println "La base de données existe déjà.")))
+
+;; Fonction pour obtenir une connexion à la base de données
+(defn get-connection []
+  (d/connect uri))
+
+;; Schéma de base pour un tuple (entité, attribut, valeur)
+(def schema
+  [{:db/ident :tuple/entity
+    :db/valueType :db.type/string
+    :db/cardinality :db.cardinality/one
+    :db/doc "Une entité pour le tuple"
+    :db.install/_attribute :db.part/db}
+   {:db/ident :tuple/attribute
+    :db/valueType :db.type/keyword
+    :db/cardinality :db.cardinality/one
+    :db/doc "Un attribut pour le tuple"
+    :db.install/_attribute :db.part/db}
+   {:db/ident :tuple/value
+    :db/valueType :db.type/string
+    :db/cardinality :db.cardinality/one
+    :db/doc "La valeur du tuple"
+    :db.install/_attribute :db.part/db}])
+
+;; Fonction pour initialiser le schéma de la base de données
+(defn init-schema [conn]
+  (d/transact conn schema))
+
+;; Fonction pour écrire un tuple (entité, attribut, valeur)
+(defn write-tuple [conn entity attribute value]
+  (let [tuple {:db/id (d/tempid :db.part/user)
+               :tuple/entity entity
+               :tuple/attribute attribute
+               :tuple/value value}]
+    (d/transact conn [tuple])))
+
+;; Fonction pour lire la dernière valeur d'un tuple par entité et attribut
+(defn read-tuple [conn entity attribute]
+  (let [db (d/db conn)
+        result (d/q '[:find ?v
+                      :in $ ?e ?a
+                      :where
+                      [?t :tuple/entity ?e]
+                      [?t :tuple/attribute ?a]
+                      [?t :tuple/value ?v]]
+                    db entity attribute)]
+    (if (seq result)
+      (ffirst result)
+      (str "Pas de valeur trouvée pour l'entité: " entity ", attribut: " attribute))))
+
+;; Initialiser la base de données et le schéma
+(defn setup []
+  (init-db)
+  (let [conn (get-connection)]
+    (init-schema conn)
+    conn))
+
+;; Exemple d'utilisation
+(defn -main []
+  (let [conn (setup)]
+    (write-tuple conn "entite1" :attribut1 "valeur1")
+    (println "Valeur pour ('entite1', :attribut1):" (read-tuple conn "entite1" :attribut1))))
